2630bd3c1aefd009c74bec9eb32ad6baf3c98dcc
anna miletova <annamiletova@gmail.com>
2025-02-11 00:03:38 +0200

    Initial project configuration

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-red-500">
+      <h1>Hierarchical Agglomerative Clustering algorithm in TypeScript</h1>
+    </div>
+  )
+}
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './assets/styles/output.css';
+import './assets/styles/index.css';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
