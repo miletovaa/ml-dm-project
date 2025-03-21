7f23ca627556f3be8e1b87b32c844240c60958a1
Мария Козлова <mariakozlova@Marias-MacBook-Air.local>
2025-03-21 20:59:00 +0100

    Initial logic of algorithm

---
+++ app/src/components/PointsScatterPlot.tsx
@@ -0,0 +1,40 @@
+import React, { useMemo } from "react"
+import { useRecoilValue } from "recoil"
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList } from 'recharts'
+import InsertNewPoint from "./InsertNewPoint"
+import { pointsState } from "../state"
+import { Points } from "../types"
+import classnames from "classnames"
+
+export default function PointsScatterPlot() {
+    const points = useRecoilValue<Points>(pointsState)
+    
+    const isPointsEmpty = useMemo(() => points?.length === 0, [points])
+
+    return (
+        <div className="flex flex-col justify-between bg-white rounded-lg bg-opacity-50 py-4 text-blue-400 transition-all duration-1000 min-w-[360px] w-1/2 opacity-100">
+            <InsertNewPoint />
+            <div className={classnames("transition-all duration-1000",{
+                "size-0": isPointsEmpty,
+                "size-80 pt-6 pr-8": !isPointsEmpty
+            })}>
+                <div className={classnames("relative transition-all duration-[2000]", {
+                    "size-0 opacity-0": isPointsEmpty,
+                    "size-full opacity-100": !isPointsEmpty
+                })}>
+                    <ResponsiveContainer width="100%">
+                        <ScatterChart>
+                            <CartesianGrid />
+                            <XAxis type="number" dataKey="x" name="X" unit="" />
+                            <YAxis type="number" dataKey="y" name="Y" unit="" />
+                            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                            <Scatter data={points} fill="#72d932">
+                                <LabelList dataKey="id" position="right" />
+                            </Scatter>
+                        </ScatterChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/src/state.ts
+++ app/src/state.ts
@@ -1,11 +1,12 @@
 import { atom } from 'recoil'
+import { Points, LinkageType } from './types'
 
-export const pointsState = atom({
+export const pointsState = atom<Points>({
   key: 'pointsState',
   default: [ ],
 });
 
-export const linkageTypeState = atom({
+export const linkageTypeState = atom<LinkageType>({
   key: 'linkageTypeState',
-  default: 0, // 0: Single, 1: Complete
+  default: 0,
 });
